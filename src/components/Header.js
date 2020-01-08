fc0abab3142f6fe60ed3eaaa921629f219eb148c
augusto pablo <augustopabloarg@gmail.com>
2020-01-09 07:59:21 +0800

    change content

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,26 @@ import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <a href="#" className="image avatar">
+            <img src={avatar} alt="" />
+          </a>
+          <h1>
+            <strong>Just call me Pablo</strong>,
+            <br /> a super React developer
+            <br />
+            with over 5+ years of experience
+            <br />
+            crafted by <a href="http://html5up.net">React.JS</a>
+          </h1>
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
