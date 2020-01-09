a7e2ab2015c63301d0b4bb4872e63691e2df5621
augusto pablo <augustopabloarg@gmail.com>
2020-01-09 12:02:35 +0800

    Change title

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,7 @@ const DEFAULT_IMAGES = [
 
 class HomeIndex extends React.Component {
   render() {
-    const siteTitle = 'Gatsby Starter - Strata'
+    const siteTitle = 'Welcome to my website'
     const siteDescription = 'Site description'
 
     return (
@@ -133,9 +133,8 @@ class HomeIndex extends React.Component {
           <section id="three">
             <h2>Get In Touch</h2>
             <p>
-              Accumsan pellentesque commodo blandit enim arcu non at amet id
-              arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi
-              lorem vulputate lorem neque lorem ipsum dolor.
+              If you like my works, send me a quick message and we can figure
+              out if we are a good fit to work together.
             </p>
             <div className="row">
               <div className="8u 12u$(small)">
@@ -195,7 +194,7 @@ class HomeIndex extends React.Component {
                     <h3 className="icon fa-envelope-o">
                       <span className="label">Email</span>
                     </h3>
-                    {/* <a href="#">hello@untitled.tld</a> */}
+                    <a href="#">augustopabloarg@gmail.com</a>
                   </li>
                 </ul>
               </div>
